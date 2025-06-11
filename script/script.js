fa443730e9291aa8a3129433d35ce21fec4ba1ca
clumsyy29 <zidanidan11@gmail.com>
2025-06-11 17:12:42 +0700

    feat: adjusment

--- script/script.js
+++ script/script.js
@@ -1,5 +1,26 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".menu-list");
 const toggleBtn = document.querySelector(".theme-toggle");
 const themeIcon = toggleBtn.querySelector("i"); // Ambil <i> di dalam tombol
+
+// nav for mobile screen
+hamburger.addEventListener("click", mobileMenu);
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+// add border when scroll down
+window.onscroll = () => {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+
+  if (window.pageYOffset > fixedNav) {
+    header.style.borderBottom = "1px solid #686868";
+  } else {
+    header.style.borderBottom = "none";
+  }
+};
 
 // state
 const theme = localStorage.getItem("theme");
@@ -7,18 +28,19 @@ const theme = localStorage.getItem("theme");
 // on mount
 if (theme) {
   document.body.classList.add(theme);
+
+  // ganti ikon jika dark-mode aktif saat halaman dimuat
   if (theme === "dark-mode") {
     themeIcon.classList.remove("fa-regular", "fa-sun");
     themeIcon.classList.add("fa-solid", "fa-moon");
   }
 }
 
-// dark theme toggle
+// dark theme
 const themeHandler = () => {
   document.body.classList.toggle("dark-mode");
-  const isDark = document.body.classList.contains("dark-mode");
 
-  if (isDark) {
+  if (document.body.classList.contains("dark-mode")) {
     localStorage.setItem("theme", "dark-mode");
     themeIcon.classList.remove("fa-regular", "fa-sun");
     themeIcon.classList.add("fa-solid", "fa-moon");
